ec16f2c9449a4e672673e8d3aa90dab0f6afc7cd
JunYoung Wang <logtve@naver.com>
2022-12-03 04:12:09 +0900

    a
    a

--- mydailyfrontapp/pages/index.tsx
+++ mydailyfrontapp/pages/index.tsx
@@ -1,13 +1,29 @@
 import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+import { SaveToLocal } from "./subPage";
+
+const hoverColor = css({
+    color: "red"
+})
+
+const hoverButton = css({
+    '&hover, &focus': hoverColor
+})
+
 
 const Button = styled.button`
-  color: hotpink;
-  width: 30%;
-  height: 200px;
+    width: 300px;
+    height: 150px;
+
 `
 
+
+
 export default function main() {
+
+
   return (
-    <Button>Button to Second page</Button>
+
+          <Button >myNewButton</Button>
   )
 }
--- mydailyfrontapp/pages/subPage.tsx
+++ mydailyfrontapp/pages/subPage.tsx
@@ -1,4 +1,36 @@
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
+import {useEffect, useReducer} from "react";
 
 
+export function SaveToLocal () {
+    const [data, dispatch] = useReducer(reducer, []);
+
+    useEffect(() => {
+        const myItem = JSON.parse(localStorage.getItem("myWords"));
+    })
+}
+
+
+const reducer = (state, action) => {
+    let newState = [];
+    switch(action.type){
+        case "INIT" : {
+            return action.data;
+        }
+        case "CREATE" : {
+            newState = [action.data, ...state];
+            break;
+        }
+        case "REMOVE" : {
+            newState = state.filter((it) => it.id !== action.targetId);
+            break;
+        }
+        case "EDIT" : {
+            newState = state.map((it) => it.id === action.data.id ? {...action.data} : it);
+            break;
+        }
+        default:
+            return state;
+    }
+}
